d82ac3b8f011af9a60995ff6dbd3734d854f6d72
ismail telhouni <ismailtelhouni123@gmail.com>
2023-04-12 04:57:13 +0000

    add la page Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FirstController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/first', name: 'app_first')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('first/index.html.twig', [
-            'controller_name' => 'FirstController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
