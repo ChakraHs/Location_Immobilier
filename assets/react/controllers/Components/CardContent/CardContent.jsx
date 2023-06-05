c4c554de159586a2c1fe42a3d354cd52c45e4124
ismail telhouni <ismailtelhouni123@gmail.com>
2023-06-05 23:14:08 +0100

    modification pour proprietaire

--- assets/react/controllers/Components/CardContent/CardContent.jsx
+++ assets/react/controllers/Components/CardContent/CardContent.jsx
@@ -6,43 +6,36 @@ import { MyDropdown } from '../index'
 
 import { HiLocationMarker } from 'react-icons/hi'
 import { MdOutlineBedroomParent ,MdOutlineBathroom } from 'react-icons/md'
-import { TbHomeEdit } from 'react-icons/tb'
 
 const CardContent = ( props ) => {
   var Bathrooms=true;
   if(props.Bathrooms==0){Bathrooms=false;}
   return (
     <div className="Annonce-item-content">
-          <div className="Annonce-item-content-immeuble">
-            <div className="Annonce-item-content-immeuble-donnee">
-              <div className="donnee category">
-                { props.Category }
-              </div>
-              <div className="donnee prix">
-                { props.Prix } MAD/mois
-              </div>
-              <div className="donnee adresse">
-                <HiLocationMarker/> { props.NumImmo } { props.Rue } , { props.Ville }
-              </div>
-              <div className="donnee intern">
-                <div className="donnee-intern"> <MdOutlineBedroomParent/> { props.Bedrooms } bedrooms</div>
-                {Bathrooms && <div className="donnee-intern"> <MdOutlineBathroom/> { props.Bathrooms } bathrooms</div>}
-                <div className="donnee-intern">{ props.Surface } m<sup>2</sup></div>
-              </div>
-            </div>
-            <div className="Annonce-item-content-immeuble-config">
-              {/* <button className="config" onClick={handleAddImageClick}>
-                <BiImageAdd/>
-              </button> */}
-              <MyDropdown editUrl={ props.editUrl } deleteUrl={ props.deleteUrl } csrf_token_id={ props.csrf_token_id } />
-              {/* <a href={ props.editUrl } className="config edit"><TbHomeEdit/></a>
-              <Deleteicon deleteUrl={ props.deleteUrl } csrf_token_id={ props.csrf_token_id } /> */}
-            </div>
+      <div className="Annonce-item-content-immeuble">
+        <div className="Annonce-item-content-immeuble-donnee">
+          <div className="donnee category">
+            { props.Category }
           </div>
-          <div className="Annonce-item-content-personel">
-            {/* {showAddImageForm && <AddImageForm addimageUrl={ props.addimageUrl } />} */}
+          <div className="donnee prix">
+            { props.Prix } MAD/mois
+          </div>
+          <div className="donnee adresse">
+            <HiLocationMarker/> { props.NumImmo } { props.Rue } , { props.Ville }
+          </div>
+          <div className="donnee intern">
+            <div className="donnee-intern"> <MdOutlineBedroomParent/> { props.Bedrooms } bedrooms</div>
+            {Bathrooms && <div className="donnee-intern"> <MdOutlineBathroom/> { props.Bathrooms } bathrooms</div>}
+            <div className="donnee-intern">{ props.Surface } m<sup>2</sup></div>
           </div>
         </div>
+        {/* <div className="Annonce-item-content-immeuble-config">
+          <MyDropdown editUrl={ props.editUrl } deleteUrl={ props.deleteUrl } csrf_token_id={ props.csrf_token_id } />
+        </div> */}
+      </div>
+      <div className="Annonce-item-content-personel">
+      </div>
+    </div> 
   )
 }
 
--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -46,9 +47,16 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        return new RedirectResponse($this->urlGenerator->generate('home_client'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        $user = $token->getUser();
+        if ($user instanceof UserInterface) {
+            $roles = $user->getRoles();
+    
+            if (in_array('ROLE_CLIENT', $roles)) {
+                return new RedirectResponse($this->urlGenerator->generate('app_home'));
+            } elseif (in_array('ROLE_PROPRIETAIRE', $roles)) {
+                return new RedirectResponse($this->urlGenerator->generate('app_annonce_index'));
+            }
+        }
     }
 
     protected function getLoginUrl(Request $request): string
